bcbf9536e9416baa5343936a98f3b3b34bf3bca6
fahminaja <nfahmi@student.ce.undip.ac.id>
2018-11-01 21:24:06 +0700

    PraktikumRSBKKel18

---
+++ src/snapapp18/SnapAPP18.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snapapp18;
+
+/**
+ *
+ * @author bleezy
+ */
+public class SnapAPP18 extends SnapFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SnapAPP18 a = new SnapAPP18();
+        a.setVisible(true);
+    }
+    
+}
